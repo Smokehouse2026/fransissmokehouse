37b85a007f730461b5db8ac86340a7ba845f21f0
NorthStarDigitalLLC <westyn@northstardigitalweb.com>
2026-04-02 08:53:49 -0500

    Update site-data.js

--- site-data.js
+++ site-data.js
@@ -110,7 +110,7 @@ var MENU = {
       {
         name:       "Brisket",
         price:      16.66,
-        img:        "Images/menu/brisket-plate.jpg",
+        img:        "Images/menu/Brisket.jpg",
         desc:       "Slow-smoked beef brisket, sliced to order. Bark-crusted, smoke-ringed, impossibly tender.",
         badge:      "Most Ordered",
         badgeStyle: "pop"
